58ffd8fe37bb729a2b6db2c4ba1dcbeb1513f1b5
GracCheng <91409177+GracCheng@users.noreply.github.com>
2021-11-11 00:07:33 +1100

    show multiple images in one card

--- frontend/src/components/HomeLists.jsx
+++ frontend/src/components/HomeLists.jsx
@@ -1,30 +1,65 @@
 import {
   Card,
   CardContent,
-  CardMedia,
+  // CardMedia,
   Typography,
+  ImageList,
+  ImageListItem,
 } from '@material-ui/core'
 import React, { useEffect, useState } from 'react'
 import fetchFunc from '../services/fetchService'
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  imageList: {
+    flexWrap: 'nowrap',
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: 'translateZ(0)',
+  },
+  title: {
+    color: theme.palette.primary.light,
+  },
+  titleBar: {
+    background:
+      'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  },
+}));
 
 const checkImageOrVedio = (thumbnail) => {
   return thumbnail.startsWith('data:image');
 }
 
+const getAllImage = (thumbnail) => {
+  const allIMage = thumbnail.split(' ');
+  allIMage.pop();
+  return allIMage;
+}
+
 function ListingBody ({ title, numReviews, price, thumbnail }) {
-  console.log(thumbnail);
+  const classes = useStyles();
   return (
+    <div>
     <Card sx={{ maxWidth: 345 }}>
       {checkImageOrVedio(thumbnail) && (
-        <>
-        <CardMedia
-          component="img"
-          height="310"
-          image={thumbnail}
-          alt="green iguana"
-        />
-        </>
+        <div className={classes.root}>
+          <ImageList className={classes.imageList} cols={1}>
+            {getAllImage(thumbnail).map((imageBase64) => {
+              return (
+                <ImageListItem key={imageBase64}>
+                <img src={imageBase64} alt="Image of listings"/>
+                </ImageListItem>
+              )
+            })}
+          </ImageList>
+        </div>
       )}
       {!checkImageOrVedio(thumbnail) && (
         <div style= { { 'text-align': 'center' } }>
@@ -54,6 +89,9 @@ function ListingBody ({ title, numReviews, price, thumbnail }) {
         <Button size="small">Learn More</Button>
       </CardActions> */}
     </Card>
+    <br />
+    </div>
+
   )
 }
 ListingBody.propTypes = {
